5760d5730c6fcd2e7c261e9a78089c6401747709
ayayushsharma <64801786+ayayushsharma@users.noreply.github.com>
2020-05-20 06:26:20 +0530

    Add files via upload

--- process_edit.php
+++ process_edit.php
@@ -14,22 +14,33 @@
    
    <div id="textbox">
     <?php                                            
-      $edit = $_POST["edit"];                        //Storing the point of grid to be marked
-	  $paswd = $_POST['paswd'];                      //Store password
+        $edit = $_POST["edit"];                        //Storing the point of grid to be marked
+	  	  
+	    $edit_in_database=0;
 	  
-	  if( $edit>=1 && $edit<=100 && $paswd == "123" )     //checks condition
+	  
+	  if( $edit>=1 && $edit<=100)     //checks condition
 		  
 		  { 
-		  $connection = new mysqli("localhost", "admin", "1234", "grid");     //Connecting mysql databases to php
-          if ($connection->connect_error) die($connection->connect_error); 
+		    $connection = new mysqli("localhost", "admin", "1234", "grid");     //Connecting mysql databases to php
+            if ($connection->connect_error) die($connection->connect_error); 
 
-          $result = $connection->query("SELECT * FROM first");                // holding the table first in $result
- 
-          if (!$result) die($connection->error);
+            $result = $connection->query("SELECT * FROM first");                // holding the table first in $result
+  
+            if (!$result) die($connection->error);
 		
-          $change="UPDATE first SET status=1 WHERE num=$edit";
+		    $result2 = $connection->query("SELECT * FROM input");                // holding the user input city in $result2
+ 
+            if (!$result2) die($connection->error); 
+
+    			$result2->data_seek(0);                           
+                $start=$result2->fetch_assoc()['start'];
+				
+				$edit_in_database=$start+$edit-1;
+		
+          $change="UPDATE first SET status=1 WHERE id=$edit_in_database";
           if(mysqli_query($connection,$change))
-			  echo 'Update Successfull.<p><a href="map.php"> Reirect to the Map </a></p>';
+			  echo 'Update Successfull.<p><a href="map.php"> Redirect to the Map </a><br><a href="homapagehtml.html"> Redirect to City Search </a></p>';
 		  else
 			  echo "Update Unsuccessfull";
 		  }
