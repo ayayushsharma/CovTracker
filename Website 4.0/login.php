3dacbfa5a9d846fc7d186df6d7f08868cfb7a967
Yashendu <62375494+Spirit-ofJoy@users.noreply.github.com>
2020-05-20 07:34:29 +0530

    Update login.php

--- Website 4.0/login.php
+++ Website 4.0/login.php
@@ -14,7 +14,8 @@
 	   
 	  <div class="login-box">        <!--Main div -->
         <h1>Login</h1>
-        
+         <a href="home.php" class="link">  Homepage</a>
+        <br> 
 		<?php
 		$seskey = $_SESSION["key"];
 		if( $seskey == '1'){
